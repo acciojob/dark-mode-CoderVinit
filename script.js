e3f30f57a9de244287276014cc9f38c39e000f7e
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-22 10:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const toggleButton = document.getElementById("toggleMode");
+const body = document.body;
+
+function setDarkMode(enabled){
+	if(enabled){
+		body.classList.add("dark-mode")
+		localStorage.setItem("darkMode","enabled")
+	}
+	else{
+		body.classList.remove("dark-mode");
+		localStorage.setItem("darkMode","disabled")
+	}
+}
+
+if (localStorage.getItem("darkMode") === "enabled") {
+    setDarkMode(true);
+}
+toggleButton.addEventListener("click", () => {
+    setDarkMode(!body.classList.contains("dark-mode"));
+});
